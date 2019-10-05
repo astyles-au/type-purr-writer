c9a6cfa038af26cc96c17dcf97f189b0aede0ba0
adam Styles <mrfawn@gmail.com>
2019-10-05 21:47:47 +1000

    added core source to this sily thing

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# type-purr-writer
+# Type-purr-writer - web app
+
 Take a moment to paws and appreciate typing on this tye-purr-writer. You may find it a-meow-sing.
+
+## Preview link
+Watch this space!
+
+## Dependencies:
+* vue JS https://vuejs.org/v2/guide/
+* Howler JS https://github.com/goldfire/howler.js
+* Google fonts - Plex https://fonts.google.com/featured/Plex
+* Font awesome 5 https://fontawesome.com
+* Animate CSS https://daneden.github.io/animate.css/
+
+## Other sources:
+* https://freesound.org/people/dancip/sounds/163286/
+
+## Contact me:
+Find me on twitter https://twitter.com/thelibstyles
---
+++ js/main.js
@@ -0,0 +1,81 @@
+// main script
+console.log('main js script loaded');
+
+
+// Howler js sound files
+var sound1fileName = '163286__dancip__meow-sample'
+var sound2fileName = '163286__dancip__meow-sample'
+
+// Howler js sound files config
+var soundDirectory = 'sounds' + '/'
+var extension = '.wav'
+var sound1 = soundDirectory + sound1fileName + extension
+var sound2 = soundDirectory + sound2fileName + extension
+
+// vue component - type-writer form
+Vue.component('type-writer', {
+    data: function () {
+        return {
+            formText: ''
+        }
+    },
+
+    // watcher
+    watch: {
+        formText: function () {
+          console.log('Some keys were pressed!')
+               // howler js sound play
+               var sound = new Howl({
+                src: [sound1],
+                autoplay: true,
+                loop: false,
+                volume: 0.5,
+                onend: function () {
+                    console.log('Finished!');
+                }
+            });
+        }
+      },
+
+    methods: {
+
+        // play typewriter ding sound via howler js - method
+        nextLine: function () {
+            console.log('You pressed enter!')
+            // howler js sound play
+            var sound = new Howl({
+                src: [sound2],
+                autoplay: true,
+                loop: false,
+                volume: 0.5,
+                onend: function () {
+                    console.log('Finished!');
+                }
+            });
+        }, // end of nextLine method
+
+    },
+    template: `
+    <form v-on:submit.prevent="onSubmit"
+    class="animated fadeInUp">
+        <textarea
+        name="typeWriterPage"
+        id="typeWriterPage"
+        cols="30" rows="10"
+        placeholder="Start typing..."
+        v-model="formText"
+        v-on:keyup.enter="nextLine"
+        >
+        </textarea>
+    </form>
+    `
+})
+
+// main vue instance
+ new Vue({
+    el: '#app',
+    data: {
+        // vueTest: 'Vue loaded ok!' // keeping this in place to test vue loaded as test
+        pageHeading: 'Type-PURR-writer'
+    }
+});
